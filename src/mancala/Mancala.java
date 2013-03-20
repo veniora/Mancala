85ce9bbe54880834c4033fb7d86fbe9041e93390
Michael Ford <veniora7@gmail.com>
2013-03-20 22:51:13 +1300

    Added instructions to repo plus a comment to self

--- src/mancala/Mancala.java
+++ src/mancala/Mancala.java
@@ -16,5 +16,7 @@ public class Mancala {
 		io.println("|  0 | 1[ 4] | 2[ 4] | 3[ 4] | 4[ 4] | 5[ 4] | 6[ 4] | P1 |");
 		io.println("+----+-------+-------+-------+-------+-------+-------+----+");
 		io.println("Player 1's turn - Specify house number or 'q' to quit: ");
+        /* This is the initial display but needs to take in and process the input*/
 	}
+
 }
